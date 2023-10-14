0ff4df201009855b9fa6c0e37ba017cec5223c22
HectorGS <A01753863@tec.mx>
2023-10-14 02:14:29 -0600

    Updated API list

--- app/src/main/java/mx/mr/platopatodos/model/ListaServiciosAPI.kt
+++ app/src/main/java/mx/mr/platopatodos/model/ListaServiciosAPI.kt
@@ -1,18 +1,24 @@
 package mx.mr.platopatodos.model
 
 import mx.mr.platopatodos.model.requests.AssistReq
+import mx.mr.platopatodos.model.requests.ChgStatusDining
 import mx.mr.platopatodos.model.requests.IncidentReq
 import mx.mr.platopatodos.model.requests.LoginReq
 import mx.mr.platopatodos.model.requests.MenuReq
 import mx.mr.platopatodos.model.requests.RegisterReq
+import mx.mr.platopatodos.model.responses.DashboardCompRes
+import mx.mr.platopatodos.model.responses.DashboardRes
 import mx.mr.platopatodos.model.responses.LoginRes
 import mx.mr.platopatodos.model.responses.RegisterRes
 import mx.mr.platopatodos.model.responses.StringResponse
 import mx.mr.platopatodos.model.responses.vulCondRes
 import retrofit2.Call
 import retrofit2.http.Body
+import retrofit2.http.GET
 import retrofit2.http.Headers
 import retrofit2.http.POST
+import retrofit2.http.PUT
+import retrofit2.http.Path
 
 interface ListaServiciosAPI {
 
@@ -41,5 +47,12 @@ interface ListaServiciosAPI {
 
     @GET("dashBoard/{nombreCom}/{fecha}")
     fun getDashboardInfo(@Path("nombreCom") diningName: String, @Path("fecha") date: String): Call<DashboardRes>
+
+    @GET("dashBoardComp/{nombreCom}/{fecha}")
+    fun getDashboardCompInfo(@Path("nombreCom") diningName: String, @Path("fecha") date: String): Call<DashboardCompRes>
+
+    @Headers("Content-Type: application/json")
+    @PUT("actualizaEstadoCom")
+    fun updateDinStatus(@Body description: ChgStatusDining): Call<StringResponse>
 
 }
