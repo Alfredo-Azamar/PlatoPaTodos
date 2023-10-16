cb10527fb15859acdd00a1c7365fb90e24bfa43f
HectorGS <A01753863@tec.mx>
2023-10-15 22:21:36 -0600

    Created QrManager object

---
+++ app/src/main/java/mx/mr/platopatodos/model/QrManager.kt
@@ -0,0 +1,20 @@
+package mx.mr.platopatodos.model
+
+import android.app.Activity
+import com.google.mlkit.vision.barcode.common.Barcode
+import com.google.mlkit.vision.codescanner.GmsBarcodeScannerOptions
+import com.google.mlkit.vision.codescanner.GmsBarcodeScanning
+
+object QrManager {
+    fun startQrCodeScanner(activity: Activity, resultCallback: (String?) -> Unit, errorCallback: (Exception) -> Unit) {
+        val options = GmsBarcodeScannerOptions.Builder()
+            .enableAutoZoom()
+            .setBarcodeFormats(Barcode.FORMAT_QR_CODE)
+            .build()
+
+        val scanner = GmsBarcodeScanning.getClient(activity, options)
+        scanner.startScan()
+            .addOnSuccessListener { barcode -> resultCallback(barcode.rawValue.toString()) }
+            .addOnFailureListener { e -> errorCallback(e) }
+    }
+}
